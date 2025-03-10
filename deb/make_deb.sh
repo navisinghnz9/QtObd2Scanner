b9e7afad7476a1c56b55daca69134ec674d84b63
Navi Singh <navisinghnz9@gmail.com>
2025-03-11 10:49:44 +1300

    DEB - generated DEBIAN/control with pkg info

--- deb/make_deb.sh
+++ deb/make_deb.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+create_control_file() {
+echo "[INFO] :: Creating DEBIAN/control file..."
+  cat > "$PACKAGE_NAME/DEBIAN/control" <<EOL
+Package: $PACKAGE_NAME
+Version: 1.0.0
+Architecture: amd64
+Maintainer: Navi Singh <navisinghnz9@gmail.com>
+Depends: python3, qt5-qmake, libqt5widgets5
+Section: utils
+Priority: optional
+Description: A Qt C++ application to perform OBD2 operations and logging
+EOL
+  echo "[INFO] :: DEBIAN/control file created."
+}
+
 PACKAGE_NAME="qtobd2scanner"
 QT_APP_BUILD_DIR="../build-QtObd2Scanner-Desktop-Debug"
 RELEASE_DIR="../deb"
 
 
-# creating the root directory for your package
+# creating the root directory for your package and
+# subdirs for bins, libs, app and startup scripts
+echo "[INFO] :: Creating package directory..."
 mkdir -p $PACKAGE_NAME/DEBIAN
-
-# creating the necessary subdirectories for binaries, libs, app and startup scripts
 mkdir -p $PACKAGE_NAME/usr/bin
 mkdir -p $PACKAGE_NAME/usr/lib/python3/dist-packages
 mkdir -p $PACKAGE_NAME/etc/init.d
@@ -16,17 +31,17 @@ mkdir -p $PACKAGE_NAME/usr/share/applications
 
 # Inform the user
 echo "========================================[ $PACKAGE_NAME ] ======================================"
-echo "Empty directory structure for .deb package '$PACKAGE_NAME' created successfully."
-echo "Now, you can add your files to the following locations:"
+echo "Directory structure for .deb package '$PACKAGE_NAME' created successfully."
 echo
 echo "  - Qt app binary:          $PACKAGE_NAME/usr/bin/"
 echo "  - Python backend server:  $PACKAGE_NAME/usr/lib/python3/dist-packages/"
 echo "  - Startup script:         $PACKAGE_NAME/etc/init.d/"
 echo "  - Desktop entry:          $PACKAGE_NAME/usr/share/applications/"
 echo
-echo "After adding your files, you can proceed with creating the .deb package."
 echo "================================================================================================"
 
+# generating the DEBIAN/control file
+create_control_file
 
 cp $QT_APP_BUILD_DIR/QtObd2Scanner $PACKAGE_NAME/usr/bin
 dpkg-deb --build $PACKAGE_NAME $RELEASE_DIR
